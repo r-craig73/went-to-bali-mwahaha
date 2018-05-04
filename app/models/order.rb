8056b2994262cff9ae58dd764785de0e4bbf91a7
Ron Craig <ron.craig@comcast.net>
2018-05-04 14:41:56 -0700

    add minor editing in index and appliction htmls, try to troubleshoot order model and understand BigDecimal

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,13 @@
 class Order < ApplicationRecord
+  require 'bigdecimal'
+
   has_many :order_items
   belongs_to :user, optional: true
 
   before_save :calculate_total
 
   def calculate_total
+    # binding.pry
     self.total_price = order_items.collect { |item| item.product.price * item.quantity }.sum
   end
 
